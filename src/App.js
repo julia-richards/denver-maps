e3821991e20f1666ed27f3c117898f8eb778ee7f
Dan <danielpowell12@msn.com>
2019-01-18 21:24:35 -0700

    Starting point
    No map yet, BUT there is a clickable neighborhood list that sets the
    activeNeighborhood in state... baby steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import neighborhoods from "./neighborhoods";
 class App extends Component {
+  state = {
+    activeNeighborhood: null,
+  };
+
+  handleNeighborhoodClick = neighborhood => {
+    this.setState({ activeNeighborhood: neighborhood });
+  };
+
   render() {
+    const { activeNeighborhood } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Logo</p>
         </header>
+        <main className="App-main">
+          <div id="map">
+            <ul>
+              {neighborhoods.map(neighborhood => (
+                <li
+                  key={neighborhood.id}
+                  onClick={this.handleNeighborhoodClick.bind(
+                    null,
+                    neighborhood
+                  )}
+                >
+                  {neighborhood.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <section className="neighborhood-detail">
+            {!!activeNeighborhood ? (
+              <React.Fragment>
+                <article>
+                  <h2>{activeNeighborhood.name}</h2>
+                  <p>{activeNeighborhood.writeUp}</p>
+                </article>
+                <aside>
+                  <h4>Key Features</h4>
+                  <ul>
+                    {activeNeighborhood.keyFeats.map((feature, i) => (
+                      <li key={i}>{feature}</li>
+                    ))}
+                  </ul>
+                </aside>
+              </React.Fragment>
+            ) : (
+              <h4>Select a neighborhood</h4>
+            )}
+          </section>
+        </main>
       </div>
     );
   }
