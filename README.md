1bc0dcb41648af22601266736a01716e8ed59500
julia-richards <julia.m.richards@gmail.com>
2019-01-29 22:29:36 -0700

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# denver-maps
-
-A basic tour and overview of neighborhoods in Denver
-
-As of Jan 2019, this is only a WIP demo that can be used as a jumping point for newcomers
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.fd34049e8602fd9451e8f7261752ede6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80f561b3bb9c23d08819",
+    "url": "/denver-maps/static/js/runtime~main.80f561b3.js"
+  },
+  {
+    "revision": "6a2f275348009f39a2ec",
+    "url": "/denver-maps/static/js/main.6a2f2753.chunk.js"
+  },
+  {
+    "revision": "28abd6be49565458b5ad",
+    "url": "/denver-maps/static/js/1.28abd6be.chunk.js"
+  },
+  {
+    "revision": "6a2f275348009f39a2ec",
+    "url": "/denver-maps/static/css/main.2dae4b71.chunk.css"
+  },
+  {
+    "revision": "522b520d556d50effd283cd4fc542e02",
+    "url": "/denver-maps/index.html"
+  }
+];
